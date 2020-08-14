f5c9ec0a80e81bbd043895121f17a644d084262e
Jaqueline Araújo <61056048+jaquearaujo906@users.noreply.github.com>
2020-08-13 23:31:18 -0300

    Initial commit

---
+++ components/tree.js
@@ -0,0 +1,34 @@
+export default function(data) {
+    const tree = document.querySelector('nav#tree')
+
+    const meni = document.createElement('ul')
+
+    const firstLevel = data.filter(item => !item.parent)
+    const getFirstLis = firstLevel.map(buildTree) 
+    getFirstLis.forEach( li => menu.append(li)) 
+
+    function buildTree(item) {
+        const li = document.createElement('li')
+        li.innerHTML = item.name
+
+        const children = data.filter(child => child.parent === item.id)
+
+        if(children.length > 0) {
+            li.addEventListener('click', event => {
+                event.stopPropagation()
+                event.target.classList.toggle('open')
+            })
+
+            li.classList.add('has-children')
+
+            const subMenu  = document.createElement('ul')
+            children.map(buildTree)
+            .forEach(li => subMenu.append(li))
+            li.append(subMenu)
+        }
+
+        return li
+    }
+
+    tree.append(menu)
+}
